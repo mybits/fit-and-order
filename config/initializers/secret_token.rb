0a8b65e4143146967589ffe753153fdbed110b29
PaulaEs <paulaproxy@gmail.com>
2013-12-06 16:30:07 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FitAndOrder::Application.config.secret_key_base = '0d5af5e8fc2aba1610ab8239ed0d3106e405009249f0103b5af5f69e6ffe2f7611f73aa65f5428dd6d25bfd387e67458beab2e22e88877b35928ef1da8a636b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FitAndOrder::Application.config.session_store :cookie_store, key: '_fit_and_order_session'
